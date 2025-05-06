a3988147caaf47f615b164b250661c758a3535ba
Eduardo Rezende <dudumoises2005@gmail.com>
2025-05-06 13:03:54 -0300

    Adding Filters into EventAggregator Publish logic
    Adding IEventFilter methods into the Publish pipeline

--- NEvent/Core/EventAggregator.cs
+++ NEvent/Core/EventAggregator.cs
@@ -3,16 +3,19 @@ using Microsoft.Extensions.Logging;
 
 namespace NEvent.Core
 {
-    public class EventAggregator : IEventAggregator
+    public sealed class EventAggregator : IEventAggregator
     {
-        private readonly ISubscriberProvider _subscriberService;
+        private readonly ISubscriberProvider _subscriberProvider;
+        private readonly IEventFilterProvider _eventFilterProvider;
         private readonly ILogger<EventAggregator>? _logger;
 
         public EventAggregator(
-            ISubscriberProvider subscriberService, 
+            ISubscriberProvider subscriberProvider,
+            IEventFilterProvider eventFilterProvider,
             ILogger<EventAggregator>? logger = null)
         {
-            _subscriberService = subscriberService;
+            _subscriberProvider = subscriberProvider;
+            _eventFilterProvider = eventFilterProvider;
             _logger = logger;
         }
 
@@ -21,16 +24,8 @@ namespace NEvent.Core
         {
             ArgumentNullException.ThrowIfNull(eventHandler, nameof(eventHandler));
 
-            ISubscriber<TEventArgs> subscriber = _subscriberService.GetSubscriber<TEventArgs>();
-
-            var type = typeof(TEventArgs);
-
-            subscriber.TryGetValue(type, out List<IEventHandler<TEventArgs>>? eventHandlers);
-
-            if (eventHandlers is null)
-                return subscriber.TryAdd([eventHandler]);
-
-            return subscriber.TryUpdate(eventHandlers);
+            ISubscriber<TEventArgs> subscriber = _subscriberProvider.Get<TEventArgs>();
+            return subscriber.TryAddOrUpdate(eventHandler);
         }
 
         public bool UnSubscribe<TEventArgs>(IEventHandler<TEventArgs> eventHandler)
@@ -38,24 +33,67 @@ namespace NEvent.Core
         {
             ArgumentNullException.ThrowIfNull(eventHandler, nameof(eventHandler));
 
-            ISubscriber<TEventArgs> subscriber = _subscriberService.GetSubscriber<TEventArgs>();
-
+            ISubscriber<TEventArgs> subscriber = _subscriberProvider.Get<TEventArgs>();
             return subscriber.TryRemove(eventHandler);
         }
 
-        public async Task PublishAsync<TEventArgs>(object sender, TEventArgs data, CancellationToken cancellationToken = default)
+        public async Task PublishAsync<TEventArgs>(object sender, TEventArgs args, CancellationToken cancellationToken = default)
             where TEventArgs : EventArgs
         {
             ArgumentNullException.ThrowIfNull(sender, nameof(sender));
-            ArgumentNullException.ThrowIfNull(data, nameof(data));
+            ArgumentNullException.ThrowIfNull(args, nameof(args));
 
-            ISubscriber<TEventArgs> subscriber = _subscriberService.GetSubscriber<TEventArgs>();
+            ISubscriber<TEventArgs> subscriber = _subscriberProvider.Get<TEventArgs>();
+            IEnumerable<IEventFilter<TEventArgs>> eventFilters = _eventFilterProvider.GetAll<TEventArgs>();
 
-            if (!subscriber.TryGetValue(typeof(TEventArgs), out List<IEventHandler<TEventArgs>>? eventHandlers))
+            if (!CanPublish(subscriber, out List<IEventHandler<TEventArgs>>? eventHandlers))
                 return;
 
-            foreach (var eventHandler in eventHandlers!)
-                await eventHandler.HandleAsync(sender, data, cancellationToken);
+            if (!eventFilters.Any())
+            {
+                await ExecuteHandlersAsync(sender, args, eventHandlers!, cancellationToken);
+                return;
+            }
+
+            foreach (IEventFilter<TEventArgs> eventFilter in eventFilters)
+            {
+                EventFilterResult filterResult = await eventFilter.OnBeforePublishAsync(sender, args, cancellationToken);
+
+                if (filterResult is EventFilterResult.CancelCompletely)
+                    break;
+
+                if (filterResult is EventFilterResult.Skip)
+                    continue;
+
+                if (filterResult is not EventFilterResult.SkipHandlers)
+                {
+                    await ExecuteHandlersAsync(sender, args, eventHandlers!, cancellationToken);
+                }
+
+                await eventFilter.OnAfterPublishAsync(sender, args, cancellationToken);
+            }
+        }       
+
+        private async Task ExecuteHandlersAsync<TEventArgs>(
+            object sender, 
+            TEventArgs args, 
+            List<IEventHandler<TEventArgs>> eventHandlers, 
+            CancellationToken cancellationToken) where TEventArgs : EventArgs
+        {
+            ArgumentNullException.ThrowIfNull(sender, nameof(sender));
+            ArgumentNullException.ThrowIfNull(args, nameof(args));
+            ArgumentNullException.ThrowIfNull(eventHandlers, nameof(eventHandlers));
+
+            foreach (IEventHandler<TEventArgs> eventHandler in eventHandlers)
+            {
+                await eventHandler.HandleAsync(sender, args, cancellationToken);
+            }
+        }
+
+        private bool CanPublish<TEventArgs>(ISubscriber<TEventArgs> subscriber, out List<IEventHandler<TEventArgs>>? eventHandlers) 
+            where TEventArgs : EventArgs
+        {
+            return subscriber.TryGetValues(typeof(TEventArgs), out eventHandlers) && eventHandlers is { Count: > 0 };
         }
     }
 }
